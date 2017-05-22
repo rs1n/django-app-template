f1f1b30309f5483792b28236fb0c3f6a5787380c
Sergey Kanukov <s.kanukov@gmail.com>
2017-05-22 15:19:20 +0300

    Translate models

--- models/foo.py
+++ models/foo.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from . import timestampable
 
 
 # A sample model.
 class Foo(timestampable.Timestampable):
-    name = models.CharField(max_length=511)
+    name = models.CharField(max_length=511, verbose_name=_('name'))
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _('foo')
+        verbose_name_plural = _('foos')
--- models/timestampable.py
+++ models/timestampable.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
-# Timestampable tracks created_at and updated_at fields.
+# Timestampable tracks 'created_at' and 'updated_at' fields.
 class Timestampable(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('created_at'))
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name=_('updated_at'))
 
     class Meta:
         abstract = True
