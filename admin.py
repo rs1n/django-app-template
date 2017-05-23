f3b2550c71aebf4c963a5f9bac6d1db25cddcb5c
Sergey Kanukov <s.kanukov@gmail.com>
2017-05-23 11:04:35 +0300

    Register an admin model for a sample one

--- admin.py
+++ admin.py
@@ -1,4 +1,9 @@
 {{ unicode_literals }}from django.contrib import admin
 
+from . import models
+
 
 # Register your models here.
+@admin.register(models.Foo)
+class FooAdmin(admin.ModelAdmin):
+    pass
