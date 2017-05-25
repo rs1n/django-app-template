9237b02345c68a7bf45abde0a39639cd30d4cfe3
Sergey Kanukov <s.kanukov@gmail.com>
2017-05-25 12:29:31 +0300

    Add a sample logging message

--- views/foo_view.py
+++ views/foo_view.py
@@ -1,10 +1,16 @@
+import logging
+
 from django import shortcuts
 
 from . import {{ app_name }}_view
+
+logger = logging.getLogger(__name__)
 
 
 class FooList({{ app_name }}_view.{{ camel_case_app_name }}View):
     'A sample view.'
 
     def get(self, request):
+        logger.info('Foo index page requested')
+
         return shortcuts.render(request, '{{ app_name }}/foo/index.html')
