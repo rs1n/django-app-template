2249dfc1ba68e4f86545e21610a5f78021d39bd2
skanukov <s.kanukov@gmail.com>
2017-09-01 13:59:25 +0300

    Format code a bit

--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from django.conf import urls
 
 from .views import foo_view
 
-
 # Define the app namespace and routes.
+
 app_name = '{{ app_name }}'
 urlpatterns = [
     urls.url(r'^$', foo_view.FooList.as_view(), name='foo_list'),
--- views/app_name_view.py
+++ views/app_name_view.py
@@ -3,5 +3,5 @@ from django.views.generic import base
 
 class {{ camel_case_app_name }}View(base.View):
     'A base class for {{ camel_case_app_name }} views.'
-    
+
     pass
